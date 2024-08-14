4eca00cebb33da75124c46ed540479f53a5bf2ed
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2024-08-14 17:50:19 -0500

    first version of the even app

---
+++ 2. even number/app/src/main/java/com/ud/evennumber/Sum.kt
@@ -0,0 +1,11 @@
+package com.ud.evennumber
+
+fun main() {
+    println("Input the first number:")
+    val number1 = readln().toInt()
+
+    println("Input the second number:")
+    val number2 = readln().toInt()
+
+    print("The sum is ${number1 + number2}")
+}
