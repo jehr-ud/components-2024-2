b0289f885d6e9de17a800119f0780c18c414bc1f
Jorge Hernandez <jehernandezr@udistrital.edu.co>
2024-09-11 17:39:10 -0500

    show the layout in activity main

--- 4. Memory Game/app/src/main/java/com/ud/memorygame/MainActivity.kt
+++ 4. Memory Game/app/src/main/java/com/ud/memorygame/MainActivity.kt
@@ -16,32 +16,6 @@ import com.ud.memorygame.ui.theme.MemoryGameTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            MemoryGameTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MemoryGameTheme {
-        Greeting("Android")
+        setContentView(R.layout.main_activity)
     }
 }
