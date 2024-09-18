9e010106f92ee7782d71bedfdc30ccee81da9657
Jorge Hernandez <jorghernandez@mineducacion.gov.co>
2024-09-18 17:31:43 -0500

    adding effect in game

--- 4. Memory Game/app/src/main/java/com/ud/memorygame/composables/MemoryGameScreen.kt
+++ 4. Memory Game/app/src/main/java/com/ud/memorygame/composables/MemoryGameScreen.kt
@@ -1,5 +1,6 @@
 package com.ud.memorygame.composables
 
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -11,12 +12,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,15 +25,26 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.ud.memorygame.logic.Game
 import com.ud.memorygame.model.enums.TypeMovement
+import kotlinx.coroutines.delay
 
 @Composable
 fun MemoryGameScreen(level: String) {
     val game = remember { Game(level) }
-    var isPlayerTurn = remember { false }
-
-    val board = game.board
+    var isPlayerTurn by remember { mutableStateOf(false) }
     var movements = game.movementSecuence
 
+    var currentMovementIndex by remember { mutableStateOf(-1) }
+
+    LaunchedEffect(movements) {
+        isPlayerTurn = false
+        movements.forEachIndexed { index, movement ->
+            currentMovementIndex = movement
+            delay(1000L)
+            currentMovementIndex = -1
+            delay(500L)
+        }
+        isPlayerTurn = true
+    }
 
     Column(
         modifier = Modifier
@@ -48,8 +60,16 @@ fun MemoryGameScreen(level: String) {
             ) {
                 for (colIndex in 0 until game.cols) {
                     val indexBoard = rowIndex * game.cols + colIndex
-                    val movement = board[rowIndex * game.cols + colIndex]
-                    MovementCard(movement, game, indexBoard)
+                    val movement = game.board[indexBoard]
+
+                    val isHighlighted = currentMovementIndex == indexBoard
+                    MovementCard(
+                        movement = movement,
+                        game = game,
+                        indexBoard = indexBoard,
+                        isClickable = isPlayerTurn,
+                        isHighlighted = isHighlighted
+                    )
                 }
             }
         }
@@ -57,13 +77,21 @@ fun MemoryGameScreen(level: String) {
 }
 
 @Composable
-fun MovementCard(movement: TypeMovement, game: Game, indexBoard: Int) {
-    Card (
+fun MovementCard(
+    movement: TypeMovement,
+    game: Game,
+    indexBoard: Int,
+    isClickable: Boolean,
+    isHighlighted: Boolean
+) {
+    val cardColor = if (isHighlighted) Color.Yellow else Color.Blue
+
+    Card(
         modifier = Modifier
             .padding(8.dp)
             .size(100.dp),
         colors = CardDefaults.cardColors(
-            containerColor = Color.Blue,
+            containerColor = cardColor,
             contentColor = Color.White
         ),
     ) {
@@ -71,7 +99,20 @@ fun MovementCard(movement: TypeMovement, game: Game, indexBoard: Int) {
             contentAlignment = Alignment.Center,
             modifier = Modifier.fillMaxSize()
         ) {
-            Button(onClick = { game.addPlayerMovement(indexBoard)}) {
+            Button(
+                onClick = {
+                    if (isClickable) {
+                        game.addPlayerMovement(indexBoard)
+                        if (game.compareMovements(indexBoard)) {
+                            Log.d("finish", "Finish the game")
+                        } else if (game.playerMovements.size == game.movementSecuence.size) {
+                            game.generateSecuence()
+                            game.playerMovements.clear()
+                        }
+                    }
+                },
+                enabled = isClickable
+            ) {
                 Text(
                     text = movement.name,
                     style = MaterialTheme.typography.labelLarge
